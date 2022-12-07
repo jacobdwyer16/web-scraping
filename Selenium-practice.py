51cca95771c23a3d6dd9ef6006e5a84ac51323d9
sjdwyer16 <62851540+sjdwyer16@users.noreply.github.com>
2022-12-06 23:23:21 -0600

    first commit

---
+++ Selenium-practice.py
@@ -0,0 +1,25 @@
+from selenium import webdriver
+import os
+from selenium.webdriver.chrome.service import Service
+
+service = Service(os.environ['driver'])
+
+def driver_maker():
+    settings = webdriver.ChromeOptions()
+    settings.add_argument("disable-infobars")
+    settings.add_argument("start-maximized")
+    settings.add_argument("disable-dev-shm-usage")
+    settings.add_argument("no-sandbox")
+    settings.add_experimental_option("excludeSwitches", ["enable-automation"])
+    settings.add_argument("disable-blink-features=AutomationControlled")
+
+    driver = webdriver.Chrome(service=service, options = settings)
+    driver.get("https://www.covenantpca.org/")
+    return driver
+
+def main():
+    driver = driver_maker()
+    element = driver.find_element(by="xpath", value=os.environ['xpath'])
+    return element.text
+
+print(main())
